3bd959d0a6128c8e0db70a01100e4f530d94b59b
Pedro Evers <pedroevers@gmail.com>
2023-06-08 23:47:20 -0300

    Criando entidades

---
+++ src/ReservaDeChromes.Domain/Entities/Horarios.cs
@@ -0,0 +1,35 @@
+﻿using ReservaDeChromes.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservaDeChromes.Domain.Entities
+{
+    public class Horarios
+    {
+        public int Id { get; set; }
+        public string Periodo { get; set; }
+
+    }
+}
+/*
+--Horarios
+1    -7:50
+2    -8:40
+3    -9:00
+4    -9:30
+5    -9:50
+6    -10:40
+7    -11:30
+8    -12:20
+9    -12:30
+10   -13:20
+11   -14:10
+12   -14:30
+13   -15:00
+14   -15:20
+15   -16:10
+16   -17:00
+*/
---
+++ src/ReservaDeChromes.Domain/Entities/Reserva.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservaDeChromes.Domain.Entities
+{
+    public class Reserva
+    {
+        public int Id { get; set; }
+        public Colaborador Responsavel { get; set; }
+        public Equipamento equipamento { get; set; }
+        public int Quantidade { get; set; }
+        public string Sala { get; set; }
+        public string Turma { get; set; }
+        public string Retirada { get; set; }
+        public string Devolução { get; set; }
+        public bool Finalizado { get; set; }
+    }
+}
